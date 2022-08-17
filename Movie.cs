272fe2c21f4a71e77cf5216fd6f862fbf6fbf262
Tricia Amheiser <tricia@taarts.net>
2022-08-17 13:27:58 -0400

    Added Movie class

---
+++ Movie.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SuncoastMoviesAgain
+{
+    public class Movie
+    {
+        public int Movie_Id { get; set; }
+        public string Title { get; set; }
+        public string PrimaryDirector { get; set; }
+        public int YearReleased { get; set; }
+        public string Genre { get; set; }
+    }
+}
